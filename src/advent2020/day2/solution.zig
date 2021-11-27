235ac9813e410d65eecbcfeaa46c1141c257a7a6
tankorsmash <tankorsmash@gmail.com>
2021-11-26 22:33:51 -0500

    remove allocPrint from day2

--- src/advent2020/day2/solution.zig
+++ src/advent2020/day2/solution.zig
@@ -30,36 +30,27 @@ pub fn solve() anyerror!void {
         //ie 13-17, s:, ssssssssssssssssgssj
         var split_iter = std.mem.split(u8, bytes, " ");
         // var split_iter = std.mem.split(u8, text, " ");
-        _ = split_iter;
 
         //ie 13-17
         var raw_count_rule = split_iter.next().?;
         var split_count_rule = std.mem.split(u8, raw_count_rule, "-");
-        _ = raw_count_rule;
 
         //ie 13
         var raw_count_min = split_count_rule.next().?;
-        _ = raw_count_min;
         var count_min: u32 = try fmt.parseInt(u32, raw_count_min[0..], 10);
-        _ = count_min;
 
         //ie 17
         var raw_count_max = split_count_rule.next().?;
-        _ = raw_count_max;
         var count_max = try fmt.parseInt(u32, raw_count_max[0..], 10);
-        _ = count_max;
 
         //ie s:
-        var raw_char_rule = split_iter.next().?;
-        _ = raw_char_rule;
+        var raw_char_rule= split_iter.next().?;
         //ie s, since its always one character
-        var char_rule: []u8 = try fmt.allocPrint(allocator, "{s}", .{raw_char_rule[0..1]});
-        _ = char_rule;
+        var char_rule = raw_char_rule[0..1];
         // std.log.info("char rule {s}", .{char_rule});
 
         //ie ssssssssssssssssgssj
         var raw_password = split_iter.next().?;
-        _ = raw_password;
 
         var char_appearances: usize = std.mem.count(u8, raw_password, char_rule);
         // @compileLog(@TypeOf(char_appearances));
