b5b04b9cffe7f395ed9550252d6cbbfb8db25c0e
tankorsmash <tankorsmash@gmail.com>
2021-11-28 15:19:07 -0500

    solve day4 part1

--- src/advent2020/day4/solution.zig
+++ src/advent2020/day4/solution.zig
@@ -36,10 +36,12 @@ pub fn solve() anyerror!void {
     var raw_lines: std.ArrayList(std.ArrayList(u8)) = std.ArrayList(std.ArrayList(u8)).init(allocator);
     _ = raw_lines;
 
-    var fields_found: i32 = 0;
+    var num_fields_found: i32 = 0;
+    var field_keys_found = std.ArrayList([]const u8).init(allocator);
+    _ = field_keys_found;
 
-    for (all_values.items) |arr_bytes, row_idx| {
-        _ = row_idx;
+    for (all_values.items) |arr_bytes, line_idx| {
+        _ = line_idx;
         var bytes: []u8 = arr_bytes.items;
         _ = bytes;
 
@@ -47,7 +49,7 @@ pub fn solve() anyerror!void {
         if (len_bytes == 0) {
             //process old passport
             for (raw_lines.items) |pp_bytes| {
-                std.log.info("processing the line: '{s}'", .{pp_bytes.items});
+                std.log.info(":: processing the line {d}:: '{s}'", .{line_idx, pp_bytes.items});
 
                 var entries = std.mem.split(u8, pp_bytes.items, " ");
                 _ = entries;
@@ -57,15 +59,17 @@ pub fn solve() anyerror!void {
                     _ = split_entries;
 
                     var key = split_entries.next().?;
+                    try field_keys_found.append(key);
                     var value = split_entries.next().?;
                     std.log.info(":: Found {s} -- value: {s}", .{ key, value });
                     if (!std.mem.eql(u8, "cid", key)) {
-                        fields_found += 1;
+                        num_fields_found += 1;
                     }
                 }
             }
 
-            if (fields_found >= 7) {
+            std.log.info("field keys found: #{d} {s}", .{num_fields_found, field_keys_found.items});
+            if (num_fields_found >= 7) {
                 std.log.info("Is valid passport", .{});
                 part1_valid_passports_found += 1;
             } else {
@@ -74,12 +78,42 @@ pub fn solve() anyerror!void {
 
             //clear old one by just replacing it
             raw_lines = std.ArrayList(std.ArrayList(u8)).init(allocator);
-            fields_found = 0;
+            field_keys_found = std.ArrayList([]const u8).init(allocator);
+            num_fields_found = 0;
             continue;
         }
         // std.log.info("length {d}", .{len_bytes});
 
         try raw_lines.append(arr_bytes);
+    }
+
+    //duplicate the checking logic for  the last item, since we skip the last one
+    for (raw_lines.items) |pp_bytes| {
+        std.log.info(":: processing the line LAST:: '{s}'", .{pp_bytes.items});
+
+        var entries = std.mem.split(u8, pp_bytes.items, " ");
+        _ = entries;
+        while (entries.next()) |entry| {
+            std.log.info("processing the entry {s}", .{entry});
+            var split_entries = std.mem.split(u8, entry, ":");
+            _ = split_entries;
+
+            var key = split_entries.next().?;
+            try field_keys_found.append(key);
+            var value = split_entries.next().?;
+            std.log.info(":: Found {s} -- value: {s}", .{ key, value });
+            if (!std.mem.eql(u8, "cid", key)) {
+                num_fields_found += 1;
+            }
+        }
+    }
+
+    std.log.info("field keys found: #{d} {s}", .{num_fields_found, field_keys_found.items});
+    if (num_fields_found >= 7) {
+        std.log.info("Is valid passport", .{});
+        part1_valid_passports_found += 1;
+    } else {
+        std.log.info("Is not a valid passport", .{});
     }
 
     std.log.info("Advent Day {d} Part 1:: {d}", .{ day, part1_valid_passports_found });
