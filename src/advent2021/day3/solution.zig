c8b85131d0abf1bcde2e89c4018c1c66051e7334
tankorsmash <tankorsmash@gmail.com>
2021-12-03 00:23:35 -0500

    first 2021 day3 part 1 failed attempt

--- src/advent2021/day3/solution.zig
+++ src/advent2021/day3/solution.zig
@@ -33,10 +33,70 @@ pub fn solve() anyerror!void {
         return;
     };
 
+    var sums = std.ArrayList(i32).init(allocator);
+    _ = sums;
+
+    //make sums 12 zeroes
+    try sums.append(0);
+    try sums.append(0);
+    try sums.append(0);
+    try sums.append(0);
+    try sums.append(0);
+    try sums.append(0);
+    try sums.append(0);
+    try sums.append(0);
+    try sums.append(0);
+    try sums.append(0);
+    try sums.append(0);
+    try sums.append(0);
+
     for (all_values.items) |line| {
         _ = line;
-    }
+        var stringed = try fmt.allocPrint(allocator, "{s}", .{line.items});
+        _ = stringed;
 
-    // std.log.info("Advent 2021 Day {d} Part 1:: {d}", .{ day, x_pos*y_pos });
+
+        for (stringed) |char, idx| {
+            // if (std.mem.eql(u8, char, "0")) {
+            if (char == '0') {
+                sums.items[idx] -= 1;
+            } else {
+                sums.items[idx] += 1;
+            }
+        }
+
+
+
+
+        // std.sort.sort(u8, &stringed, {}, comptime std.sort.asc(u8));
+        //
+        // const first_one = std.mem.indexOf(u8, stringed, '1').?;
+        // _ = first_one;
+        //
+        // if (first_one >= 6) {
+        //
+        // }
+
+    }
+    var g_bit_0: u8 = if (sums.items[0] > 0 ) '1' else '0';
+    _ = g_bit_0;
+    var g_bit_1: u8 = if (sums.items[1] > 0 ) '1' else '0';
+    _ = g_bit_1;
+    var g_bit_2: u8 = if (sums.items[2] > 0 ) '1' else '0';
+    _ = g_bit_2;
+    var g_bit_3: u8 = if (sums.items[3] > 0 ) '1' else '0';
+    _ = g_bit_3;
+    var g_bit_4: u8 = if (sums.items[4] > 0 ) '1' else '0';
+    _ = g_bit_4;
+
+    var gamma_str = [_]u8 { g_bit_0, g_bit_1, g_bit_2, g_bit_3, g_bit_4};
+
+    var gamma: i32 = try std.fmt.parseInt(i32, gamma_str[0..], 2); //how the fuck
+    _ = gamma;
+
+    var epsilon = 1 ^ gamma;
+    _ = epsilon;
+
+    std.log.info("Advent 2021 Day {d} Part 1:: {d}", .{ day, gamma*epsilon});
     // std.log.info("Advent 2021 Day {d} Part 2:: {d}", .{ day, horiz*depth });
 }
