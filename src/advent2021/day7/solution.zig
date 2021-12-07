0a1c1dd5c9c61b3dc2fd7b0e3bc9222965d9be90
tankorsmash <tankorsmash@gmail.com>
2021-12-07 12:20:05 -0500

    fmt

--- src/advent2021/day7/solution.zig
+++ src/advent2021/day7/solution.zig
@@ -3,7 +3,9 @@ const File = std.fs.File;
 const fmt = std.fmt;
 const info = std.log.info;
 
-pub fn log(comptime format: []const u8,) void { info(format, .{}); }
+pub fn log(comptime format: []const u8) void {
+    info(format, .{});
+}
 
 const load_input = @import("./../../advent2020/shared/load_input.zig");
 
@@ -36,12 +38,14 @@ pub fn parse_i32(bytes: []const u8) std.fmt.ParseIntError!i32 {
     return value;
 }
 
-pub const CrabCount = struct { pos : i32, count: i32};
+pub const CrabCount = struct { pos: i32, count: i32 };
 
-pub fn sort_crabs_by_count (desc: bool, left: CrabCount, right:CrabCount) bool {
-// pub fn sort_crabs_by_count (left: CrabCount, right:CrabCount) bool {
+pub fn sort_crabs_by_count(desc: bool, left: CrabCount, right: CrabCount) bool {
+    // pub fn sort_crabs_by_count (left: CrabCount, right:CrabCount) bool {
     // _ = context;
-    if (desc) { return left.count > right.count; }
+    if (desc) {
+        return left.count > right.count;
+    }
 
     return left.count < right.count;
 }
@@ -66,8 +70,8 @@ pub fn solve() anyerror!void {
         while (splitter.next()) |raw_crab| {
             var crab = try parse_i32(raw_crab);
 
-            var counted:?i32 = counter_map.get(crab);
-            var new_counted : i32 = undefined;
+            var counted: ?i32 = counter_map.get(crab);
+            var new_counted: i32 = undefined;
             if (counted != null) {
                 new_counted = counted.? + 1;
             } else {
@@ -84,31 +88,31 @@ pub fn solve() anyerror!void {
     var counter_map_it = counter_map.iterator();
     while (counter_map_it.next()) |counted| {
         // info("counter_map:: {d} - {d}", .{counted.key_ptr.*, counted.value_ptr.*});
-        try crab_count.append(CrabCount{.pos = counted.key_ptr.*, .count= counted.value_ptr.*});
+        try crab_count.append(CrabCount{ .pos = counted.key_ptr.*, .count = counted.value_ptr.* });
     }
 
-    const min_crab_pos:i32 = std.mem.min(i32, crabs.items);
-    const max_crab_pos:i32 = std.mem.max(i32, crabs.items);
-    info("min {d} max {d}", .{min_crab_pos, max_crab_pos});
+    const min_crab_pos: i32 = std.mem.min(i32, crabs.items);
+    const max_crab_pos: i32 = std.mem.max(i32, crabs.items);
+    info("min {d} max {d}", .{ min_crab_pos, max_crab_pos });
 
-    var dest:i32 = 0;
-    var part1_smallest_total_fuel: i32 =  999999999;
-    var part2_smallest_total_fuel: i32 =  999999999;
-    while (dest <= max_crab_pos) : (dest += 1)  {
-        var part1_total_fuel:i32 = 0;
-        var part2_total_fuel:i32 = 0;
-        for (crabs.items) | crab_pos | {
-            var d:i32 = try std.math.absInt(crab_pos - dest);
+    var dest: i32 = 0;
+    var part1_smallest_total_fuel: i32 = 999999999;
+    var part2_smallest_total_fuel: i32 = 999999999;
+    while (dest <= max_crab_pos) : (dest += 1) {
+        var part1_total_fuel: i32 = 0;
+        var part2_total_fuel: i32 = 0;
+        for (crabs.items) |crab_pos| {
+            var d: i32 = try std.math.absInt(crab_pos - dest);
             part1_total_fuel += d;
 
-            part2_total_fuel += @divExact((d * (d+1)), @intCast(i32, 2));
+            part2_total_fuel += @divExact((d * (d + 1)), @intCast(i32, 2));
         }
         part1_smallest_total_fuel = std.math.min(part1_smallest_total_fuel, part1_total_fuel);
         part2_smallest_total_fuel = std.math.min(part2_smallest_total_fuel, part2_total_fuel);
     }
 
     std.log.info("Advent 2021 Day {d} Part 1:: {d}", .{ day, part1_smallest_total_fuel });
-    std.log.info("Advent 2021 Day {d} Part 2:: {d}", .{ day, part2_smallest_total_fuel});
+    std.log.info("Advent 2021 Day {d} Part 2:: {d}", .{ day, part2_smallest_total_fuel });
 
     std.log.info("done", .{});
 }
