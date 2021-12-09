5691d00b0498fd181180a832f4ae432451d96af8
tankorsmash <tankorsmash@gmail.com>
2021-12-09 00:33:54 -0500

    attempt failed 2021 day 9 part 1

--- src/advent2021/day9/solution.zig
+++ src/advent2021/day9/solution.zig
@@ -39,6 +39,60 @@ pub fn parse_i32(bytes: []const u8) std.fmt.ParseIntError!i32 {
     return value;
 }
 
+const cols: i32 = 100;
+
+pub fn rev_lookup(coord: i32) [2]i32 {
+    // const rows: i32 = 990 + 1;
+    // const rows: i32 = 9 + 1;
+    // const cols: i32 = 9 + 1;
+    _ = cols;
+
+    // x
+
+    // coord = (cols * y) + x;
+    var x: i32 = @mod(coord, cols);
+    var y: i32 = @divFloor((coord - x), cols);
+
+    return [2]i32{ x, y };
+}
+pub fn lookup(x: i32, y: i32) i32 {
+    // const rows: i32 = 990 + 1;
+    // const rows: i32 = 9 + 1;
+    // const cols: i32 = 9 + 1;
+    _ = cols;
+
+    return cols * y + x;
+}
+
+const offsets = [_][2]i32{
+    [2]i32{ -1, -1 },
+    [2]i32{ -1, 0 },
+    [2]i32{ -1, 1 },
+    [2]i32{ 0, -1 },
+    // [2]i32{0, 0},
+    [2]i32{ 0, 1 },
+    [2]i32{ 1, -1 },
+    [2]i32{ 1, 0 },
+    [2]i32{ 1, 1 },
+};
+
+pub fn is_lower_than_neighbors(map: std.ArrayList(u8), x: usize, y: usize, cur_val: u8) bool {
+    for (offsets) |offset| {
+        var ox:i32 = @intCast(i32, x) + offset[0];
+        var oy:i32 = @intCast(i32, y) + offset[1];
+        var offset_idx:i32 = lookup(ox, oy);
+        if (offset_idx <= std.mem.len(map.items) and offset_idx >= 0) {
+            var offset_val = map.items[@intCast(usize, offset_idx)];
+
+            if (cur_val >= offset_val) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
 pub fn solve() anyerror!void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -51,12 +105,27 @@ pub fn solve() anyerror!void {
         return err;
     };
 
+    var height_map = std.ArrayList(u8).init(allocator);
+
     for (all_values.items) |line| {
         _ = line;
-
+        for (line.items) |digit| {
+            try height_map.append(digit);
+        }
     }
 
-    // std.log.info("Advent 2021 Day {d} Part 1:: {d}", .{ day, unique_digits_found });
+    var total_risk_level: i32 = 0;
+
+    _ = total_risk_level;
+    for (height_map.items) |height, height_idx| {
+        _ = height;
+        var xy = rev_lookup(@intCast(i32, height_idx));
+        if (is_lower_than_neighbors(height_map, @intCast(usize, xy[0]), @intCast(usize, xy[1]), height)) {
+            total_risk_level += height + 1;
+        }
+    }
+
+    std.log.info("Advent 2021 Day {d} Part 1:: {d}", .{ day, total_risk_level }); //not 34128
     // std.log.info("Advent 2021 Day {d} Part 2:: {d}", .{ day, running_total });
 
     std.log.info("done", .{});
